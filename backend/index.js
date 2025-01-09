bc737a9ff6961f345d117af70865f68e9459ad0b
Yash Makwana <yashmakwana6353@gmail.com>
2025-01-09 21:37:49 +0530

    Boilerplate Setup Done

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser())
+
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+// app.use('/users', userRoutes);
+// app.use('/captains', captainRoutes);
+
+module.exports = app;
